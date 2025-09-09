ae04ac2c729f58b52c4ecbecb9b9e06da4823525
esther apalara <apalaraesther@gmail.com>
2025-09-09 23:09:07 +0100

    Add 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,6 +1,6 @@
-const firstArg = process.argv[2];
-if (firstArg === undefined) {
-     console.log ("No argument");
+const args = process.argv.slice(2); 
+if (args[0] === undefined) {
+     console.log("No argument");
 } else {
-     console.log (firstArg);
+     console.log(args.join(" "));
 }
